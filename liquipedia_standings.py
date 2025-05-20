fcb9dd5410e90fe19fee196a4859c1add523e2af
jeromehage <jeromehage>
2025-05-20 07:05:30 +0200

    improve tiebreaker maths

--- liquipedia_standings.py
+++ liquipedia_standings.py
@@ -3,9 +3,12 @@ import pandas as pd
 import os
 import copy
 
+from sklearn.preprocessing import MinMaxScaler
+
 ## CONFIG
 
-resolve_ties = True
+resolve_ties = True # calculate tiebreaker points based on score vs teams above
+check_map_result = True # slightly more points for ex, a 2-0 win than 2-1
 
 # allows: None, 'up', 'stay', 'down', 'top_6'
 stay = 'stay'
@@ -13,6 +16,15 @@ stay = 'stay'
 
 
 ## MAIN
+
+def scale(arr, rng = (0, 1)):
+    arr = np.array(arr)
+    if (arr == arr[0]).all():
+        v = (rng[0] + rng[1]) * 0.5
+        return np.repeat(v, arr.shape[0])
+    else:
+        scaler = MinMaxScaler(feature_range = rng)
+        return scaler.fit_transform(arr.reshape(-1, 1)).reshape(-1)
 
 # read back liquipedia matches section and parse it
 # not super pretty but it works
@@ -22,12 +34,12 @@ with open(os.path.join('groups', 'matches.txt'), encoding = 'utf-8') as f:
 # table of results to fill in from liquipedia
 results = {}
 default = {'win': [], 'tie': [], 'lose': []}
-def add_result(results, team, r, opp):
+def add_result(results, team, r, opp, score = (0, 0)):
     if team not in results:
         results[team] = copy.deepcopy(default) # python is by reference
     if r not in default.keys():
         raise KeyError('result r: {} is invalid}'.format(r))
-    results[team][r] += [opp]
+    results[team][r] += [(opp, score)]
 
 # parse liqupedia match list
 weeks = matches.split('{{Matchlist')[1:]
@@ -51,8 +63,12 @@ for week in weeks:
                 else:
                     winner = 1
                 loser = 1 - winner
-                add_result(results, teams[winner], 'win', teams[loser])
-                add_result(results, teams[loser], 'lose', teams[winner])
+                # add win
+                score = (wincount[winner], wincount[loser])
+                add_result(results, teams[winner], 'win', teams[loser], score)
+                # add loss
+                score = (wincount[loser], wincount[winner])
+                add_result(results, teams[loser], 'lose', teams[winner], score)
             else:
                 # tie
                 add_result(results, teams[0], 'tie', teams[1])
@@ -89,44 +105,65 @@ ties = ties['team'].agg(list)
 ties = ties[ties.index != -1]
 ties.sort_index(inplace = True)
 
-data['pos'] = data.index
+data['pos'] = data.index + 1
+data['pos_'] = data.index + 1 # internal, uses decimal team place
 data.loc[data['tied'], 'pos'] = data.loc[data['tied'], 'ties']
 
 # tiebreakers
 if resolve_ties == True:
 
-    ## TODO: check head to head first
+    ## TODO: also check head-to-head
 
     # sum leaderboard ranks of teams above as points
     # resolve with the tied team with most points
     tiebreaker_points_factor = {
-        'win': 2,
-        'tie': 1,
-        'lose': -1,
+        'win': 2, # points for a win per nb of positions above
+        'tie': 1, # points for a tie per nb of positions above
+        'lose': -1, # negative points for a loss per reverse pos
+        'map': 0.5, # map win score max impact on tb points [0.0 1.0]
         }
     tbpf = tiebreaker_points_factor # name too long
 
     for pos, tie in ties.items():
         teams_above = data[data.index < pos]
-        print(pos, teams_above)
+        print('Tiebreaker for place:', pos + 1)
+        print('Teams:', tie)
+        print('Teams above:\n', teams_above)
+        print('Matches:')
         tb_points = {}
         for team in tie:
             points = 0
 
-            for t in results[team]['win']:
+            for t, sc in results[team]['win']:
+                print(team, 'W', sc, t)
                 if t in teams_above['team'].values:
-                    t_pos = data[data['team'] == t].iloc[0]['pos']
-                    points += tbpf['win'] * (pos - t_pos) # always > 0
+                    pen = 1
+                    if check_map_result:
+                        # add penalty for map result (ex: 2-1 vs 2-0)
+                        pen = sc[0] / sum(sc)
+                        pm = tbpf['map']
+                        pen = pen * pm + (1 - pm)
+                    # gain more points for higher win
+                    t_pos = data[data['team'] == t].iloc[0]['pos_']
+                    points += tbpf['win'] * pen * (pos - t_pos) # always > 0
 
-            for t in results[team]['tie']:
+            for t, sc in results[team]['tie']:
                 if t in teams_above['team'].values:
-                    t_pos = data[data['team'] == t].iloc[0]['pos']
+                    t_pos = data[data['team'] == t].iloc[0]['pos_']
                     points += tbpf['tie'] * (pos - t_pos)
 
-            for t in results[team]['lose']:
+            for t, sc in results[team]['lose']:
+                print(team, 'L', sc, t)
                 if t in teams_above['team'].values:
-                    t_pos = data[data['team'] == t].iloc[0]['pos']
-                    points += tbpf['lose'] * (0 + t_pos) # its negative
+                    pen = 1
+                    if check_map_result:
+                        # add penalty for map result (ex: 1-2 vs 0-2)
+                        pen = sc[1] / sum(sc)
+                        pm = tbpf['map']
+                        pen = pen * pm + (1 - pm)
+                    # lose less points for higher loss
+                    t_pos = data[data['team'] == t].iloc[0]['pos_']
+                    points += tbpf['lose'] * pen * (0 + t_pos) # its negative
 
             tb_points[team] = points
 
@@ -149,6 +186,13 @@ if resolve_ties == True:
                 pts_cnt += 1
                 resolution += [pts_cnt]
         tbpt['pos'] = resolution
+        tbpt['pos'] += 1
+
+        # also calculate internal decimal team places
+        n = len(tbpt) # is len(tie)
+        scaled_tbp = scale(tbpt['tbp'], (0, n - 1))
+        tbpt['pos_'] = pos + n - scaled_tbp
+        print('Tiebreaker points table:\n', tbpt)
 
         # update standings table before going to next tie
         for i, res in tbpt.iterrows():
@@ -156,9 +200,13 @@ if resolve_ties == True:
             idx = data[data['team'] == res['team']].index
             data.loc[idx, 'pos'] = res['pos']
             data.loc[idx, 'tbp'] = res['tbp']
+            data.loc[idx, 'pos_'] = res['pos_']
         data = data.sort_values('pos').reset_index(drop = True)
 
-data['pos'] = data['pos'] + 1
+        print('')
+
+print('Final table:')
+data['pos'] = data['pos']
 print(data, end = '\n\n')
 
 header = """==Group Standings==
